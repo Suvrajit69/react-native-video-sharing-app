9a7646ed005a1601d80403f2744ad39ee9069693
Suvrajit Mondal <suvrajitmondal359@gmail.com>
2024-07-02 18:17:49 +0530

    fix: input focus border fixed

--- components/FormField.jsx
+++ components/FormField.jsx
@@ -15,7 +15,7 @@ const FormField = ({
   return (
     <View className={`space-y-2 ${otherStyles}`}>
       <Text className="text-base text-gray-100 font-medium">{title}</Text>
-      <View className="w-full h-16 px-4 bg-black-100 rounded-2xl focus:border-secondary items-center border-2 border-black-200 flex-row">
+      <View className="flex flex-row items-center space-x-4 w-full h-16 px-4 bg-black-100 rounded-2xl border-2 border-black-200 focus:border-secondary-100">
         <TextInput
           className="flex-1 text-white font-psemibold text-base"
           value={value}
@@ -25,7 +25,7 @@ const FormField = ({
           secureTextEntry={title === "Password" && !showPassword}
         />
         {title === "Password" && (
-          <TouchableOpacity onPress={() => setShowPassword((prev)=>!prev)}>
+          <TouchableOpacity onPress={() => setShowPassword((prev) => !prev)}>
             <Image
               source={showPassword ? icons.eye : icons.eyeHide}
               resizeMode="contain"
--- components/SearchInput.jsx
+++ components/SearchInput.jsx
@@ -1,43 +1,36 @@
-import {
-  View,
-  Text,
-  TextInput,
-  TouchableOpacity,
-  Image,
-  Alert,
-} from "react-native";
-import React, { useState } from "react";
+import { useState } from "react";
+import { router, usePathname } from "expo-router";
+import { View, TouchableOpacity, Image, TextInput, Alert } from "react-native";
 
 import { icons } from "../constants";
-import { router, usePathname } from "expo-router";
 
-const SearchInput = ({initialQuery}) => {
-  // const [showPassword, setShowPassword] = useState(false);
-  const [query, setQuery] = useState(initialQuery || "");
+const SearchInput = ({ initialQuery }) => {
   const pathname = usePathname();
+  const [query, setQuery] = useState(initialQuery || "");
 
   return (
-    <View className="w-full h-16 px-4 bg-black-100 rounded-2xl focus:border-secondary items-center border-2 border-black-200 flex-row space-x-4">
+    <View className="flex flex-row items-center space-x-4 w-full h-16 px-4 bg-black-100 rounded-2xl border-2 border-black-200 focus:border-secondary-100">
       <TextInput
         className="text-base mt-0.5 text-white flex-1 font-pregular"
         value={query}
-        placeholder="Search for a video topic"
+        placeholder="Search a video topic"
         placeholderTextColor="#CDCDE0"
         onChangeText={(e) => setQuery(e)}
       />
+
       <TouchableOpacity
         onPress={() => {
-          if (!query) {
+          if (query === "")
             return Alert.alert(
-              "Missing query",
+              "Missing Query",
               "Please input something to search results across database"
             );
-          }
+
           if (pathname.startsWith("/search")) router.setParams({ query });
           else router.push(`/search/${query}`);
         }}
       >
-        <Image source={icons.search} resize="contain" className="w-5 h-5" />
+        <Image source={icons.search} className="w-5 h-5" resizeMode="contain" />
       </TouchableOpacity>
     </View>
   );
