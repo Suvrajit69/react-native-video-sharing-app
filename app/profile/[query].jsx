091857f5d21d55205bf20c8794caf3c0948d4629
Suvrajit Mondal <suvrajitmondal359@gmail.com>
2024-07-05 18:00:25 +0530

    fix: Image source null doesn't exist and production configuration

--- app/profile/[query].jsx
+++ app/profile/[query].jsx
@@ -7,7 +7,7 @@ import {
 } from "react-native";
 import EmptyState from "../../components/EmptyState";
 import { StatusBar } from "expo-status-bar";
-import { getUserPosts, getUserProfile} from "../../lib/appwrite";
+import { getUserPosts, getUserProfile } from "../../lib/appwrite";
 import useAppwrite from "../../lib/useAppwrite";
 import VideoCard from "../../components/VideoCard";
 import InfoBox from "../../components/InfoBox";
@@ -15,12 +15,15 @@ import { useState } from "react";
 import Modal from "../../components/Modal";
 import { useLocalSearchParams } from "expo-router";
 import { Entypo } from "@expo/vector-icons";
+import { icons } from "../../constants";
 
 const Profile = () => {
   const { query } = useLocalSearchParams();
   const { data: posts } = useAppwrite(() => getUserPosts(query));
   const { data: user } = useAppwrite(() => getUserProfile(query));
   const [modalOpen, setModalOpen] = useState(false);
+
+  const userAvatar = user?.avatar ? { uri: user.avatar } : icons.profile;
 
   return (
     <SafeAreaView className="bg-primary flex-1">
@@ -35,7 +38,7 @@ const Profile = () => {
               onPress={() => setModalOpen((prev) => !prev)}
             >
               <Image
-                source={{ uri: user?.avatar }}
+                source={userAvatar}
                 className="w-[90%] h-[90%] rounded-lg"
                 resizeMode="cover"
               />
@@ -45,7 +48,7 @@ const Profile = () => {
               <View className="w-[80%] h-[45%] bg-primary justify-center items-center rounded-md relative">
                 <View className="w-32 h-32 border border-secondary border-dashed rounded-lg justify-center items-center">
                   <Image
-                    source={{ uri: user?.avatar }}
+                    source={userAvatar}
                     className="w-[90%] h-[90%] rounded-lg"
                     resizeMode="cover"
                   />
@@ -65,7 +68,7 @@ const Profile = () => {
               containerStyles="mt-5"
               titleStyles="text-lg"
             />
-            <View className="flex-row ">
+            <View className="flex-row">
               <InfoBox
                 title={posts.length || 0}
                 subTitle="Posts"
@@ -94,3 +97,4 @@ const Profile = () => {
 };
 
 export default Profile;
+
